106e345ca27de67b04542e4538bb96dbbb37d46d
Celine Brin <celine-brin@hotmail.fr>
2022-11-15 15:56:13 +0100

    article

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,21 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root "articles#index"
+
+  # CREATE
+    get "articles/new", to: "articles#new", as: "new_article"
+    post "articles", to: "articles#create"
+
+  # READ
+    get "articles", to: "articles#index"
+    get "articles/:id", to: "articles#show", as: "article"
+
+  # UPDATE
+    get "articles/:id/edit", to: "articles#edit", as: "edit_article"
+    patch "articles/:id", to: "articles#update"
+
+  # DELETE
+    delete "articles/:id", to: "articles#destroy"
+
 end
